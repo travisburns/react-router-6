6f6e47215d9e35a717eb9d15526727247c17d8a7
Travis <travisjb0@gmail.com>
2023-08-17 18:24:23 -0700

    first pages

--- react-router-6-tutorial-main/src/App.js
+++ react-router-6-tutorial-main/src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<div>Home Page</div>} />
+    <Route path="testing" element={<div>
+      <h2>testing</h2>
+    </div>} />
+   </Routes>
+   
+   
+   </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
