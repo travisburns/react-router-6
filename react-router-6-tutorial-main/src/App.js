a3e5070de5ad6bec4d9065ded986c422bd31c3d6
Travis <travisjb0@gmail.com>
2023-08-17 18:48:00 -0700

    Error page Refresh

--- react-router-6-tutorial-main/src/App.js
+++ react-router-6-tutorial-main/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
+import Error from './pages/Error'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <Route path="/" element={<Home />} />
     <Route path="about" element={<About />} />
     <Route path="products" element={<Products />} />
+    <Route path="*" element={<Error />} />
    </Routes>
    
    
--- react-router-6-tutorial-main/src/pages/Error.js
+++ react-router-6-tutorial-main/src/pages/Error.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>Error 404: Not found.</h2>
+      <h3>These are not the Droids you are looking for. </h3>
+      <Link to ="/">Back Home</Link>
     </section>
   );
 };
