1d389b2a6b6d06e9a3bbe83bef283780be879389
Travis <travisjb0@gmail.com>
2023-08-17 18:35:02 -0700

    Page components

--- react-router-6-tutorial-main/src/App.js
+++ react-router-6-tutorial-main/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
 
 const App = () => {
   return (
    <BrowserRouter>
    <Routes>
-    <Route path="/" element={<div>Home Page</div>} />
-    <Route path="testing" element={<div>
-      <h2>testing</h2>
-    </div>} />
+    <Route path="/" element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="products" element={<Products />} />
    </Routes>
    
    
