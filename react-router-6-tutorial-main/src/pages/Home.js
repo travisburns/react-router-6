f74b75fc47fe308756a7629419b51933ab7970c0
Travis <travisjb0@gmail.com>
2023-08-17 19:41:57 -0700

    Shared layout

--- react-router-6-tutorial-main/src/pages/Home.js
+++ react-router-6-tutorial-main/src/pages/Home.js
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
-
+import { Link, Outlet} from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
 
 
   return (
+
+    <>
+    <Navbar/>
+    
     <section className='section'>
       <h2>Home Page</h2>
-      <Link to = "/about" className="btn">About</Link>
+      <Outlet />
     </section>
+
+    </>
+    
   );
 };
 export default Home;
